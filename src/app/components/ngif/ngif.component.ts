96911a1f2cefd1ddd52a58777f4c2a14cf02989d
Josue Darce <darcejosue30@gmail.com>
2023-07-29 09:07:42 -0600

    ngIf NgFor

---
+++ src/app/components/ngif/ngif.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngif',
+  templateUrl: './ngif.component.html',
+  styleUrls: ['./ngif.component.css']
+})
+export class NgifComponent {
+  nombre = 'Josue ';
+  edad = 17
+;
+  sueldos = [10_000, 15_000, 19_000];
+}
